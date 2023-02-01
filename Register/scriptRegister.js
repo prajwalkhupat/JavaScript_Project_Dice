47f84e53e8c2d1084fcc942c1cff930fb8237b17
prajwal khupat <prajwalkhupat5@gmail.com>
2023-02-01 23:22:40 +0530

    completed code

---
+++ Register/scriptRegister.js
@@ -0,0 +1,24 @@
+
+function passValues(){
+    const fname = document.getElementById("fname").value;
+    const uname = document.getElementById("uname").value;
+    const email = document.getElementById("email").value;
+    
+    localStorage.setItem("textname",fname);
+    localStorage.setItem("textusername",uname);
+    
+    return false;
+
+}
+
+document.getElementById("resultname").innerHTML = localStorage.getItem("textname");
+document.getElementById("resultusername").innerHTML = localStorage.getItem("textusername");
+
+
+function fun(){
+    document.getElementById("resultname").value='';
+    document.getElementById("resultusername").value='';
+    document.getElementById("email").value='';
+
+}
+   
